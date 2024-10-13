2edbb06d0fa9b51d5aec559bb037ce98fd2f72c0
ne_srv <ne_srv@mail.ru>
2024-10-13 12:15:35 +0300

    autosave

--- lesson11 (orm-p7 many-to-many tables by orm).py
+++ lesson11 (orm-p7 many-to-many tables by orm).py
@@ -1,6 +1,6 @@
 import psycopg2
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Identity
+from sqlalchemy import Column, Integer, String, Identity, Text
 from sqlalchemy.orm import DeclarativeBase, Session
 
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
@@ -20,8 +20,14 @@ class Person(Base):
     __tablename__ = "books"
 
     id = Column(Integer, Identity(always=True), primary_key=True)
-    title  = Column(String)
+    title  = Column(Text, nullable=False)
 
+
+class AuthorShip(Base):
+    __tablename__ = "authorship"
+    book_id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, primary_key=True)
+    seq_num = Column(Integer, nullable=False)
 
 
 Base.metadata.create_all(bind=engine)
